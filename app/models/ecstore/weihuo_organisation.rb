06014ac30d16fc66c98f7706d63b10673a1a9eb1
Scenery Ye <scenery.ye@gmail.com>
2015-11-10 16:59:51 +0800

    weihuo admin

--- app/models/ecstore/weihuo_organisation.rb
+++ app/models/ecstore/weihuo_organisation.rb
@@ -3,6 +3,7 @@ class Ecstore::WeihuoOrganisation < Ecstore::Base
   self.table_name  = 'weihuo_organisations'
   self.accessible_all_columns
 
-  has_many :weihuo_employee,:foreign_key=>"weihuo_organisation_id"
+  has_many :weihuo_employees,:foreign_key=>"weihuo_organisation_id"
+  has_many :weihuo_shops,:foreign_key=>"weihuo_organisation_id"
 
 end
--- app/models/ecstore/weihuo_shop.rb
+++ app/models/ecstore/weihuo_shop.rb
@@ -6,7 +6,7 @@ class Ecstore::WeihuoShop < Ecstore::Base
   
   belongs_to :weihuo_organisation,:foreign_key=>"weihuo_organisation_id"
 
-  has_many :weihuo_shops_good
+  has_many :weihuo_shops_goods
   has_many :good, :through => :weihuo_shops_good
   has_one :weihuo_employee, :foreign_key => 'shop_id'
   belongs_to :user, :foreign_key=>"member_id"
