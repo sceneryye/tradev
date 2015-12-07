a1495d4ce48f98e2a69c7b72bd6d940a5cf64ca4
存在的虚无 <killernova@126.com>
2015-12-07 10:21:53 +0800

    add foodie_pay

---
+++ app/controllers/foodies_controller.rb
@@ -0,0 +1,60 @@
+class FoodiesController < ApplicationController
+  def foodie_pay
+    supplier = Ecstore::Supplier.where(:name => '贸威').first
+    weixin_appid = supplier.weixin_appid
+    weixin_appsecret = supplier.weixin_appsecret
+    mch_id = supplier.mch_id
+    attach = "#{params[:event_id]}_#{params[:participant_id]}_#{params[:user_id]}"
+    nonce_str = random_str 32
+    out_trade_no = Time.new.to_i.to_s + rand(10 ** 10).to_s.rjust(10, '0')
+    body = params[:event_name]
+    spbill_create_ip = '182.254.138.119'
+    trade_type = 'JSAPI'
+    notify_url = 'http://www.trade-v.com/weihuo/notify_page'
+    post_data_hash = {:appid => weixin_appid, :mch_id => mch_id, :nonce_str => nonce_str, :body => body, :out_trade_no => out_trade_no, :total_fee => params[:money], :attach => attach, :spbill_create_ip => spbill_create_ip, :notify_url => notify_url, :trade_type => trade_type}
+    sign = create_sign post_data_hash
+    post_data_hash[:sign] = sign
+    post_data_xml = to_label_xml post_data_hash
+
+    post_url = 'https://api.mch.weixin.qq.com/pay/unifiedorder'
+    res_data_hash = Hash.from_xml(RestClient.post post_url, post_data_xml)
+    return render :text => res_data_hash
+  end
+end
+
+private
+
+def to_label_xml hash
+  params_str = ''
+  hash.each do |key, value|
+    params_str += "<#{key}>" + "<![CDATA[#{value}]]>" + "</#{key}>"
+  end
+  params_xml = '<xml>' + params_str + '</xml>'
+end
+
+def random_str str_length
+  arr = ('0'..'9').to_a + ('a'..'z').to_a
+  nonce_str = ''
+  str_length.times do
+    nonce_str += arr[rand(36)].upcase
+  end
+  nonce_str
+end
+
+def create_sign hash
+  key = Ecstore::Supplier.where(:name => '贸威').first.partner_key
+  stringA = hash.select{|key, value|value.present?}.sort.map do |arr|
+   arr.map(&:to_s).join('=')
+ end
+ stringA = stringA.join("&")
+ string_sing_temp = stringA + "&key=#{key}"
+ sign = (Digest::MD5.hexdigest string_sing_temp).upcase
+end
+
+def access_token
+  supplier = Ecstore::Supplier.where(:name => '贸威').first
+  weixin_appid = supplier.weixin_appid
+  weixin_appsecret = supplier.weixin_appsecret
+  get_access_token = RestClient.get "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{weixin_appid}&secret=#{weixin_appsecret}"
+  access_token = ActiveSupport::JSON.decode(get_access_token)['access_token']
+end
