49b3ad38bfd1d8fe337077249cc0f5de30f34280
killernova <enternicky@126.com>
2016-01-06 20:26:24 +0800

    bug

--- config/initializers/letter_avatar.rb
+++
@@ -1,4 +0,0 @@
-LetterAvatar.setup do |config|
-  config.cache_base_path = 'public/system/lets' # default is 'public/system'
-  config.colors_palette = :iwanthue             # default is :google
-end
