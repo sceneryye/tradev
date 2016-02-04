864cd34d7ac4ce764b8bbe4d6419eece4ce123d3
killernova <enternicky@126.com>
2016-02-04 15:46:16 +0800

    add reflash_token to attr_accessible in auth_ext.rb

--- app/models/ecstore/auth_ext.rb
+++ app/models/ecstore/auth_ext.rb
@@ -2,7 +2,7 @@ class Ecstore::AuthExt < Ecstore::Base
 	self.table_name = "sdb_pam_auth_ext"
 	belongs_to :account,:foreign_key=>"account_id"
 
-	attr_accessible :account_id,:access_token,:expires_at,:expires_in,:uid,:provider
+	attr_accessible :account_id,:access_token,:expires_at,:expires_in,:uid,:provider, :refresh_token
 
 	def expired?
 		expires_at < Time.zone.now.to_i
